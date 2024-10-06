a315c84e9d2d8fcad7257abeb43e115db8ba4e71
vedhatiAdarsh <116135413+vedhatiAdarsh@users.noreply.github.com>
2024-10-06 15:52:15 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let sub=document.querySelector('button')
+let v1=document.querySelector('#c1').value
+let v2=document.querySelector('#c2').value
+let v3=document.querySelector('#c3').value
+let div=document.querySelector('div')
+sub.addEventListener('click',(e)=>{
+    e.preventDefault();
+    
+    console.log(v1,v2,v3);
+        
+}
+)
